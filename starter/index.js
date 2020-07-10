331ffd2d453ca38a1802d573040a0e2b79cf93e4
Bryan M <allen.bry95@gmail.com>
2020-07-10 11:22:07 +0200

    Asynchronous write data

--- starter/index.js
+++ starter/index.js
@@ -23,12 +23,20 @@ const fs = require('fs');
  * 3rd param: callback
  * the file will be read in the background without blocking the thread
 */
+
+//! Callback hell
 fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
     // this console.log will show the data once the file is fully read
     console.log(data2);
     fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
       console.log(data3);
+
+      // Asynchronous Write data, no need of the data param in the callback since
+      // we don't read any
+      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
+        console.log('Your file has been written !')
+      });
     });
   });
 });
