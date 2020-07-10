6b7a93c469bdc074cd5765d3813eee7cd88137b3
Bryan M <allen.bry95@gmail.com>
2020-07-10 11:13:43 +0200

    Asynchronous read data

--- starter/index.js
+++ starter/index.js
@@ -1,15 +1,30 @@
 // import fs (file system) module
 const fs = require('fs');
 
-// Read a file
-// First param: file path
-// 2nd param: encoding
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+//== Read/Write datas in synchronous way
+/*
+ * Read a file (synchronous)
+ * First param: file path
+ * 2nd param: encoding
+*/ 
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 
-const textOut = `This is what we know about the avocado: ${textIn}\nCreated on ${Date.now()}`;
+// const textOut = `This is what we know about the avocado: ${textIn}\nCreated on ${Date.now()}`;
+/* 
+ * Write a file (synchronous)
+ * First param: File path
+ * 2nd param, string to write
+*/
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log('File written !');
 
-// Write a file
-// First param: File path
-// 2nd param, string to write
-fs.writeFileSync('./txt/output.txt', textOut);
-console.log('File written !');
+//== Read/Write datas in asynchronous way
+/*
+ * 3rd param: callback
+ * the file will be read in the background without blocking the thread
+*/
+fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
+  // this console.log will show the data once the file is fully read
+  console.log(data);
+});
+console.log('Will read file')
