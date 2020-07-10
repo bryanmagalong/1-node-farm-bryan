b5e7a357285c0e93adc151449b5533d6d96a4230
Bryan M <allen.bry95@gmail.com>
2020-07-10 11:39:29 +0200

    First server

--- starter/index.js
+++ starter/index.js
@@ -1,6 +1,7 @@
-// import fs (file system) module
 const fs = require('fs');
+const http = require('http');
 
+//========= FILE
 //== Read/Write datas in synchronous way
 /*
  * Read a file (synchronous)
@@ -24,22 +25,40 @@ const fs = require('fs');
  * the file will be read in the background without blocking the thread
 */
 //! Callback hell
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-  // error handling
-  if(err) return console.log('ERROR!');
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//   // error handling
+//   if(err) return console.log('ERROR!');
   
-  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-    // this console.log will show the data once the file is fully read
-    console.log(data2);
-    fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
-      console.log(data3);
+//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//     // this console.log will show the data once the file is fully read
+//     console.log(data2);
+//     fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+//       console.log(data3);
 
-      // Asynchronous Write data, no need of the data param in the callback since
-      // we don't read any
-      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
-        console.log('Your file has been written !')
-      });
-    });
-  });
+//       // Asynchronous Write data, no need of the data param in the callback since
+//       // we don't read any
+//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
+//         console.log('Your file has been written !')
+//       });
+//     });
+//   });
+// });
+// console.log('Will read file')
+//=========
+
+//========= SERVER
+/**
+ * Each time we hit the server, the callback function will be called
+ * In this callback, we have access to the Request and Response object
+ */
+const server = http.createServer((req, res) => {
+  res.end('Hello World from the server !');
 });
-console.log('Will read file')
+
+// Specify which port the server will listen
+// 1rst param: port
+// 2nd param: host (default is localhost -> 127.0.0.1) opt
+// callback opt
+server.listen(8001, '127.0.0.1', () => {
+  console.log('Listening to request on port 8001');
+});
