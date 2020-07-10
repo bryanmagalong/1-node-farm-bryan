17a3a2a23d145bca023b088b630192b70bfcec4b
Bryan M <allen.bry95@gmail.com>
2020-07-10 11:16:54 +0200

    multiple callback read file

--- starter/index.js
+++ starter/index.js
@@ -23,8 +23,13 @@ const fs = require('fs');
  * 3rd param: callback
  * the file will be read in the background without blocking the thread
 */
-fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
-  // this console.log will show the data once the file is fully read
-  console.log(data);
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+    // this console.log will show the data once the file is fully read
+    console.log(data2);
+    fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+      console.log(data3);
+    });
+  });
 });
 console.log('Will read file')
