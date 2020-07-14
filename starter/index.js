2547ca1beaad90d621bcce9c5a003d37e57a84d2
Bryan M <allen.bry95@gmail.com>
2020-07-14 11:50:45 +0200

    prettierrc configuration, format changes

--- starter/index.js
+++ starter/index.js
@@ -12,15 +12,16 @@ const replaceTemplate = require('./modules/replaceTemplate');
  * Read a file (synchronous)
  * First param: file path
  * 2nd param: encoding
-*/ 
+ */
+
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 
 // const textOut = `This is what we know about the avocado: ${textIn}\nCreated on ${Date.now()}`;
-/* 
+/*
  * Write a file (synchronous)
  * First param: File path
  * 2nd param, string to write
-*/
+ */
 // fs.writeFileSync('./txt/output.txt', textOut);
 // console.log('File written !');
 
@@ -28,12 +29,12 @@ const replaceTemplate = require('./modules/replaceTemplate');
 /*
  * 3rd param: callback
  * the file will be read in the background without blocking the thread
-*/
+ */
 //! Callback hell
 // fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
 //   // error handling
 //   if(err) return console.log('ERROR!');
-  
+
 //   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
 //     // this console.log will show the data once the file is fully read
 //     console.log(data2);
@@ -55,16 +56,25 @@ const replaceTemplate = require('./modules/replaceTemplate');
 /**
  * Each time we hit the server, the callback function will be called
  * In this callback, we have access to the Request and Response object
-*/
+ */
 
 // We read the data once, only in the beginning
 // and each time we hit the api route, we send it back
 // __dirname -> where the current file is located
 
 //== html templates
-const templateOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
-const templateCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
-const templateProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const templateOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const templateCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const templateProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
@@ -80,12 +90,14 @@ const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
 
   // if the url is the root or overview
-  if(pathname === '/' || pathname === '/overview') {
+  if (pathname === '/' || pathname === '/overview') {
     res.writeHead(200, { 'Content-type': 'text/html' });
-    
+
     // Replace all placceholders for each card in dataObj
     // and join all the elements of the new array in a string
-    const cardsHtml = dataObj.map((elem) => replaceTemplate(templateCard, elem)).join('');
+    const cardsHtml = dataObj
+      .map((elem) => replaceTemplate(templateCard, elem))
+      .join('');
 
     // Replace the PRODUCT_CARDS placeholder with our html
     const output = templateOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
@@ -99,18 +111,15 @@ const server = http.createServer((req, res) => {
     const output = replaceTemplate(templateProduct, product);
     res.end(output);
   } else if (pathname === '/api') {
-
-
     res.writeHead(200, { 'Content-type': 'application/json' });
     res.end(data);
-  }
-  else {
+  } else {
     // error 404
 
     // send an http header
     // we specify that the content that we're sending is html
     res.writeHead(404, {
-      'Content-type': 'text/html'
+      'Content-type': 'text/html',
     });
     res.end('<h1>PAGE NOT FOUND</h1>');
   }
